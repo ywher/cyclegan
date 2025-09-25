6ab9bf737baeb4fe10e2f9a1c9a573e2495178ee
ywher <958118196@qq.com>
2025-09-25 15:54:44 +0800

    update city2dark code

--- train.sh
+++ train.sh
@@ -2,5 +2,14 @@
 # python train.py --dataroot ./datasets/maps --name maps_cyclegan --model cycle_gan --use_wandb
 
 # cityscapes
-python train.py --dataroot ./datasets/cityscapes_seg_2_img --name cityscapes_cyclegan --model cycle_gan 
+# CUDA_VISIBLE_DEVICES=4 python train.py --dataroot ./datasets/cityscapes_seg_2_img --name cityscapes_cyclegan --model cycle_gan 
 # --use_wandb
+
+# city 2 acdc dark
+CUDA_VISIBLE_DEVICES=0 python train.py \
+--dataroot ./datasets/city2dark \
+--name city2dark_cyclegan \
+--model cycle_gan \
+--preprocess scale_width_and_crop \
+--load_size 1024 \
+--crop_size 512
